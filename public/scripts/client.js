4d911b2320b2b5edf057a1c78880e5f1a16e8386
ZuhraB <zuhra.bakhshi.com>
2020-09-09 21:50:08 +0000

    added the Ajs instead of HTML in the index.html file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,34 +3,67 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-}
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
 const createTweetElement = (userData) => {
-  
   let html = `<article class="tweet">
-    name: ${userData.user.name}
-    avatars: ${userData.user.avatars}
-    handle: ${userData.user.handle}
-    created_at:${userData.created_at}
+  <header>
+            <div class="photo">
+              <img class="profile" src="${userData.user.avatars}"> 
+              <p> <h3>${userData.user.name}</h3> </p>
+              </div>
+                <div>
+                <p class="email"> ${userData.user.handle} </p>
+              </div>
+          </header> 
+          <div class="message">
+            <p>${userData.content.text}</p>
+        </div>
+          <footer class="days-imgs">
+            <p class="days">  ${userData.created_at} </p>
+            <div>
+              <img src="photos/flag-alt-solid-24.png">
+              <img src="photos/heart-solid-24.png">
+              <img src="photos/repost-regular-24.png"> 
+            </div>
+          </footer>
   </article>`
-  return $('#tweets-container').append(html); 
+  return html;
 }
-console.log(createTweetElement(tweetData))
-const $tweet = createTweetElement(tweetData);
-$('#tweets-container').append($tweet); 
 
-const renderTweets = () => {
-
+const renderTweets = (tweetData) => {
+  for (let tweet of tweetData) {
+    const newTweet = createTweetElement(tweet)
+    console.log(newTweet)
+    $('#tweets-container').append(newTweet);
+  }
 }
+$( document ).ready(function() {
+  renderTweets(data);
+});
+
 
 
 
