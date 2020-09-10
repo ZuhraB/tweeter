9a8f5696c10bdd39b58223530b62e55ee1bbf194
ZuhraB <zuhra.bakhshi.com>
2020-09-10 16:43:32 +0000

    Added Cross-Site Scripting

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,20 +3,25 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escape =  function(str) {
+  let div = document.createElement('div');
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+}
 
 const createTweetElement = (userData) => {
   let html = `<article class="tweet">
   <header>
             <div class="photo">
               <img class="profile" src="${userData.user.avatars}"> 
-              <p> <h3>${userData.user.name}</h3> </p>
+                <p> <h3>${userData.user.name}</h3> </p>
               </div>
                 <div>
                 <p class="email"> ${userData.user.handle} </p>
               </div>
           </header> 
           <div class="message">
-            <p>${userData.content.text}</p>
+            <p>${escape(userData.content.text)}</p>
         </div>
           <footer class="days-imgs">
             <p class="days">  ${userData.created_at} </p>
@@ -31,23 +36,27 @@ const createTweetElement = (userData) => {
 }
 
 const renderTweets = (tweetData) => {
+  $('#tweets-container').empty()
   for (let tweet of tweetData) {
     const newTweet = createTweetElement(tweet)
-    $('#tweets-container').append(newTweet);
+    $('#tweets-container').prepend(newTweet);
   }
 }
 $( document ).ready(function() {
   $(".new-tweet-form").on("submit", function(event){
-   event.preventDefault()
-    $.ajax({url: '/tweets' , method: 'POST', data: $(this).serialize()}).then(response =>{
-      if (response.length > 0 && response.length <= 140) {
-      loadTweets();
-      } else if (response.length > 140) {
-        alert ("You just hit the maximum limit!!!")
-      } 
-    }).catch(e => {
-      alert("Somthing went wrong, you might have forgot to tweet!!")
-    })
+   event.preventDefault();
+   const tweetText = $("#tweet-text").val()
+   if (tweetText.length > 0 && tweetText.length <= 140) {
+     $.ajax({url: '/tweets' , method: 'POST', data: $(this).serialize()}).then(response =>{
+       loadTweets();
+     }).catch(e => {
+       alert("Somthing went wrong!!")
+     })
+  } else if (tweetText.length > 140) {
+    alert ("You just hit the maximum limit!!!")
+  } else {
+    alert ("Please tweet!!")
+  }
   });
   const loadTweets = () => {
     $.ajax({url: ' http://localhost:8080/tweets' , method: 'GET'}).then(response =>{
