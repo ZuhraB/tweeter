2211686601f226375d7b6509f7cde760af6be7a6
ZuhraB <zuhra.bakhshi.com>
2020-09-10 00:17:56 +0000

    Added the GET request for the form submission

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,7 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 const createTweetElement = (userData) => {
   let html = `<article class="tweet">
   <header>
@@ -60,15 +37,21 @@ const renderTweets = (tweetData) => {
   }
 }
 $( document ).ready(function() {
-  renderTweets(data);
   $(".new-tweet-form").on("submit", function(event){
    event.preventDefault()
     $.ajax({url: '/tweets' , method: 'POST', data: $(this).serialize(), success: (response) =>{
-      console.log("it worked")
-      console.log(response)
+      loadTweets();
     }});
   });
+  const loadTweets = () => {
+    $.ajax({url: ' http://localhost:8080/tweets' , method: 'GET'}).then(response =>{
+      renderTweets(response)
+      console.log(response)
+      })
+  }
+  loadTweets();
 });
+
 
 
 
